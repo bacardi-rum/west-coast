a50a5170987e0c652aa6030c63fae890a18c1434
沈聪 <shencong2001@live.cn>
2022-07-03 13:06:27 +0800

    fix https

--- config/server.config.ts
+++ config/server.config.ts
@@ -1,4 +1,3 @@
 export default {
-  // SERVER: process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://westcaost.blue:8000'
-  SERVER: 'https://localhost:8000'
+  SERVER: process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://westcaost.blue'
 }
